9d7fbbaa49fa25cab19cbac6fb3bc9238fb9f7d6
Miko-4343 <mihako43@gmail.com>
2026-07-14 23:28:50 +0200

    If this works i will go to sleep and no more pushes else, you will see anoter one

--- app/server.js
+++ app/server.js
@@ -181,6 +181,10 @@ app.get('/health', (req, res) => {
 });
 
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/api', (req, res) => {
   res.status(200).json({
     message: 'Hello from Kubernetess!',
     hostname: process.env.HOSTNAME,
