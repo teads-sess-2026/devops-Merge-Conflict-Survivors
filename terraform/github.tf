26e636b659a681ea0c4ed10ef60ee441f2791499
Miko-4343 <mihako43@gmail.com>
2026-07-14 13:27:48 +0200

    added github.tf

---
+++ terraform/github.tf
@@ -0,0 +1,50 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1b511abead59c6ce207077c0ef0285192718a61b"
+  ]
+
+  tags = { Name = "github-actions-oidc-Merge-Conflict" }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role"
+  permissions_boundary = "arn:aws:iam::937697200280:policy/summer-school-ljubljana-boundary"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:teads-sess-2026/teads-summer-school-2026:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = { Name = "github-actions-role-Merge-Conflict" }
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  value       = aws_iam_role.github_actions.arn
+  description = "ARN for GitHub Actions to assume - add to GitHub secrets as AWS_ROLE_ARN"
+}
