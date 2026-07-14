374158e83ed9ff295459d46d93b16a1c77c38a84
Blaz Pirc <blaz.pirc04@gmail.com>
2026-07-14 15:01:42 +0200

    access changed to API_AND_CONFIG_MAP

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -86,6 +86,11 @@ resource "aws_eks_cluster" "main" {
     endpoint_public_access  = true
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [aws_iam_role_policy_attachment.eks_cluster]
 
   tags = { Name = "eks-merge-conflict-survivors" }
